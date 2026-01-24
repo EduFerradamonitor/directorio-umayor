696a0f2b01572919e98c854a98cb2d812b537158
EduFerradamonitor <eduardo.ferrada.ext@gmail.com>
2026-01-23 22:25:03 -0300

    Add home route and connect main cover

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify, url_for
 from supabase import create_client
 
 app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 # =========================
 # SUPABASE
@@ -274,3 +277,4 @@ function limpiar() {{
 # =========================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
